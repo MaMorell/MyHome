6667d60db8145b69888a247ed4086c70d0654e7a
Morell, Mattias <mattias.morell@if.se>
2025-02-23 14:20:24 +0100

    Handle token exceptions

--- MyHome.Core/Repositories/FloorHeating/FloorHeaterRepository.cs
+++ MyHome.Core/Repositories/FloorHeating/FloorHeaterRepository.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Options;
 using MyHome.Core.Options;
 using System.Text.Json;
+using static com.clusterrr.TuyaNet.TuyaApi;
 
 namespace MyHome.Core.Repositories.FloorHeating;
 
@@ -15,7 +16,7 @@ public class FloorHeaterRepository
     public FloorHeaterRepository(IOptions<FloorHeaterOptions> options)
     {
         _tuyaApi = new TuyaApi(
-            region: TuyaApi.Region.CentralEurope,
+            region: Region.CentralEurope,
             accessId: options.Value.AccessId,
             apiSecret: options.Value.ApiSecret
         );
@@ -25,12 +26,14 @@ public class FloorHeaterRepository
 
     public async Task<double> GetSetTemperatureAsync()
     {
-        var statusJson = await _tuyaApi.RequestAsync(TuyaApi.Method.GET, $"/v1.0/devices/{_deviceId}/status");
+        var statusJson = await RequestAsync(Method.GET, $"/v1.0/devices/{_deviceId}/status");
 
-        var status = JsonSerializer.Deserialize<List<ThermostatStatus>>(statusJson, new JsonSerializerOptions()
+        var jsonOptions = new JsonSerializerOptions()
         {
             PropertyNameCaseInsensitive = true
-        });
+        };
+        var status = JsonSerializer.Deserialize<List<ThermostatStatus>>(statusJson, jsonOptions) 
+            ?? throw new InvalidOperationException($"Failed to parse data: {statusJson}");
 
         var tempSet = status.FirstOrDefault(x => x.Code == "temp_set")?.Value.GetInt32()
             ?? throw new ThermostatDeviceException("Temperature setting not found in device status");
@@ -57,16 +60,27 @@ public class FloorHeaterRepository
         {
             commands = new[]
             {
-                new
-                {
-                    code = CODE_TEMP_SET,
-                    value = scaledTemprature
-                }
+            new
+            {
+                code = CODE_TEMP_SET,
+                value = scaledTemprature
             }
+        }
         };
-
         var commandJson = JsonSerializer.Serialize(command);
-        await _tuyaApi.RequestAsync(TuyaApi.Method.POST, $"/v1.0/devices/{_deviceId}/commands", commandJson);
+        await RequestAsync(Method.POST, $"/v1.0/devices/{_deviceId}/commands", commandJson);
+    }
+
+    private async Task<string> RequestAsync(Method method, string uri, string? body = null)
+    {
+        try
+        {
+            return await _tuyaApi.RequestAsync(method, uri, body);
+        }
+        catch (InvalidDataException e) when (e.Message.Contains("token invalid"))
+        {
+            return await _tuyaApi.RequestAsync(method, uri, body, forceTokenRefresh: true);
+        }
     }
 
     private static int ConvertToScaledTemp(int temperature)
