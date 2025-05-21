efb9b67638738c28ccb3a202317f0a89a440a61c
Morell, Mattias <mattias.morell@if.se>
2025-05-21 21:14:29 +0200

    Colors price list

--- MyHome.Web/Components/Pages/EnergyLists.razor.cs
+++ MyHome.Web/Components/Pages/EnergyLists.razor.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components;
-using MyHome.Core.Models.EnergySupplier;
+using MudBlazor;
+using MyHome.Core.Models.EnergySupplier.Enums;
 using MyHome.Web.ExternalClients;
 
 namespace MyHome.Web.Components.Pages;
@@ -8,7 +9,7 @@ public partial class EnergyLists
     [Inject]
     private EnergySupplierClient EnergySupplierClient { get; set; } = default!;
 
-    private IEnumerable<EnergyConsumptionEntry> prices = [];
+    private IEnumerable<EnergyListItem> _prices = [];
     private bool _loading;
 
     protected override async Task OnInitializedAsync()
@@ -22,12 +23,58 @@ public partial class EnergyLists
 
             await Task.WhenAll(pricesTask, waitTask);
 
-            prices = await pricesTask;
+            var prices = await pricesTask;
+
+            _prices = prices.Select(p => new EnergyListItem()
+            {
+                Consumption = p.Consumption,
+                Cost = p.Cost,
+                LevelExternal = p.PriceDetails.LevelExternal,
+                LevelInternal = p.PriceDetails.LevelInternal,
+                PriceTotal = p.PriceDetails.PriceTotal,
+                StartsAt = p.PriceDetails.StartsAt,
+            }).ToList();
         }
         finally
         {
             _loading = false;
         }
-
     }
+}
+
+public class EnergyListItem
+{
+    public decimal Consumption { get; set; }
+    public decimal Cost { get; set; }
+    public decimal PriceTotal { get; set; }
+    public DateTimeOffset StartsAt { get; init; }
+    public EnergyPriceLevel LevelExternal { get; set; }
+    public EnergyPriceLevel LevelInternal { get; set; }
+
+    public string LevelExternalColor 
+    {
+        get
+        {
+            return GetPriceLevelColor(LevelExternal);
+        }
+    }
+
+    public string LevelInternalColor
+    {
+        get
+        {
+            return GetPriceLevelColor(LevelInternal);
+        }
+    }
+
+    private static string GetPriceLevelColor(EnergyPriceLevel level) => level switch
+    {
+        EnergyPriceLevel.VeryCheap => Colors.Green.Lighten1,
+        EnergyPriceLevel.Cheap => Colors.LightGreen.Lighten1,
+        EnergyPriceLevel.Normal => Colors.Gray.Lighten1,
+        EnergyPriceLevel.Expensive => Colors.Yellow.Lighten1,
+        EnergyPriceLevel.VeryExpensive => Colors.Orange.Lighten1,
+        EnergyPriceLevel.Extreme => Colors.Red.Lighten1,
+        _ => Colors.Gray.Darken2,
+    };
 }
