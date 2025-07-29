808b5fdd134b677d0b203debd59665770e96ba34
Morell, Mattias <mattias.morell@if.se>
2025-07-29 11:38:22 +0200

    Bug fix

--- MyHome.Web/ExternalClients/AuditClient.cs
+++ MyHome.Web/ExternalClients/AuditClient.cs
@@ -6,7 +6,7 @@ public class AuditClient(ApiServiceClient client)
 {
     public async Task<IEnumerable<AuditEvent>> GetAuditEventsAsync(CancellationToken cancellationToken = default)
     {
-        var result = await client.GetFromJsonAsync<IEnumerable<AuditEvent>>($"auditevents?count={20}", cancellationToken);
+        var result = await client.GetFromJsonAsync<IEnumerable<AuditEvent>>($"auditevents?limit={20}", cancellationToken);
 
         return result ?? [];
     }
